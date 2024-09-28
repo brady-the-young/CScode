6d63536995314717d55e78061dafc5b9792f2b6a
Brady Young <142329285+brady-the-young@users.noreply.github.com>
2024-09-28 15:33:56 -0500

    Add files via upload

---
+++ BradyWeb/code.ts
@@ -0,0 +1,19 @@
+function toggleDropdown() {
+    const dropdown = document.getElementById("myDropdown");
+    if (dropdown) {
+        dropdown.classList.toggle("show");
+    }
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (event.target && !(event.target as Element).matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
